a8114457688308d46c942ae43512a40a07de02a5
artem-dev <artem.didenkojs@gmail.com>
2021-10-18 16:52:25 +0300

    done

--- src/schemas/post.schema.ts
+++ src/schemas/post.schema.ts
@@ -5,19 +5,12 @@ var faker = require('faker');
 
 @Schema()
 export class Post extends Document {
-  // @Factory will automatically inject faker to the function that you pass.
-  // @Factory(faker => faker.random.arrayElement(['male', 'female']))
-  // @Prop({ required: true })
-  // gender: string;
-
-  // You could get the previously generated value using the passed context.
   @Factory(() => {
     return faker.internet.domainName();
   })
   @Prop({ required: true })
   domainName: string;
 
-  // You could also use custom function without faker.
   @Factory(() => {
     const minAge = 1;
     const maxAge = 1000;
@@ -26,25 +19,11 @@ export class Post extends Document {
   @Prop({ required: true })
   ownerId: string;
 
-  // You could get the previously generated value using the passed context.
   @Factory(() => {
     return faker.name.findName();
   })
   @Prop({ required: true })
   ownerName: string;
-
-
-
-  // You could also use static value.
-  // @Factory('admin')
-  // @Prop({ required: true })
-  // role: string; 
-
-  // If you pass predefined values to the `generate` function, you will be 
-  // able to access it in the context.
-  // @Factory((faker, ctx) => `${faker.address.streetAddress()} ${ctx.zipCode}`)
-  // @Prop({ required: true })
-  // address: string;
 }
 
 export const userSchema = SchemaFactory.createForClass(Post);
--- src/seeders/post.seeder.ts
+++ src/seeders/post.seeder.ts
@@ -9,12 +9,7 @@ export class UsersSeeder implements Seeder {
   constructor(@InjectModel(Post.name) private readonly post: Model<Post>) {}
 
   async seed(): Promise<any> {
-    // Generate 10 users.
-    // Notice that we can optionally pass values that will be accessible in
-    // the context that's passed to the @Factory decorator.
     const post = DataFactory.createForClass(Post).generate(10, { zipCode: '10153' });
-
-    // Insert into the database.
     return this.post.insertMany(post);
   }
 
